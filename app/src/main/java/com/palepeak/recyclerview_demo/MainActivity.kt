a6c02421e9d7297c4229738964f324c5e9091efa
Bobby Wang <bobby.wang25@gmail.com>
2021-01-06 11:30:37 -0500

    simple recyclerview finished

--- app/src/main/java/com/palepeak/recyclerview_demo/MainActivity.kt
+++ app/src/main/java/com/palepeak/recyclerview_demo/MainActivity.kt
@@ -2,10 +2,42 @@ package com.palepeak.recyclerview_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // The data
+        val data = listOf(
+                ListEntries("Entry 1", "Test"),
+                ListEntries("Entry 2", "Test"),
+                ListEntries("Entry 3", "Test"),
+                ListEntries("Entry 4", "Test"),
+                ListEntries("Entry 5", "Test"),
+                ListEntries("Entry 6", "Test"),
+                ListEntries("Entry 7", "Test"),
+                ListEntries("Entry 8", "Test"),
+                ListEntries("Entry 9", "Test"),
+                ListEntries("Entry 10", "Test"),
+                ListEntries("Entry 11", "Test"),
+                ListEntries("Entry 12", "Test"),
+                ListEntries("Entry 13", "Test"),
+                ListEntries("Entry 14", "Test"),
+                ListEntries("Entry 15", "Test"),
+                ListEntries("Entry 16", "Test"),
+                ListEntries("Entry 17", "Test"),
+                ListEntries("Entry 18", "Test")
+        )
+
+        // The View
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        // The Adapter
+        recyclerView.adapter = MyAdapter(data)
+
     }
+
+    data class ListEntries(val entry1: String, val entry2: String)
 }
---
+++ app/src/main/java/com/palepeak/recyclerview_demo/MyAdapter.kt
@@ -0,0 +1,43 @@
+package com.palepeak.recyclerview_demo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class MyAdapter(private val dataSet: List<MainActivity.ListEntries>) :
+    RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView = view.textView
+        val textView2 = view.textView2
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.list_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = dataSet[position].entry1
+        viewHolder.textView2.text = dataSet[position].entry2
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
